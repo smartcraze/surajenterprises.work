79cea81132a1064e4047914a28ecc1cba3a95caf
suraj vishwakarma <smartcraze354@gmail.com>
2025-10-12 17:50:36 +0530

    refactor: remove unused components and files
    - Deleted components.json, Navbar.tsx, button.tsx, navigation-menu.tsx, sheet.tsx, and utils.ts as they are no longer needed.
    - Updated package.json to reflect changes in dependencies and scripts.
    - Removed unused images from the public directory.
    - Adjusted ESLint configuration to ignore specific directories.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { MainNav } from "@/components/Navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,9 +25,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased dark`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <MainNav/>
         {children}
       </body>
     </html>
